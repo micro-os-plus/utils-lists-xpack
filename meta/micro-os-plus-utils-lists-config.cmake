1d9be6184b3212eb26ebfc72df0b4f4e22f9f6be
Liviu Ionescu <ilg@livius.net>
2021-03-04 10:28:20 +0200

    cmake: => prefix

--- meta/micro-os-plus-utils-lists-config.cmake
+++ meta/micro-os-plus-utils-lists-config.cmake
@@ -66,7 +66,7 @@ if(NOT TARGET micro-os-plus-utils-lists-static)
   # Aliases.
 
   add_library(micro-os-plus::utils-lists-static ALIAS micro-os-plus-utils-lists-static)
-  message(STATUS "micro-os-plus::utils-lists-static")
+  message(STATUS "=> micro-os-plus::utils-lists-static")
 
 endif()
 
