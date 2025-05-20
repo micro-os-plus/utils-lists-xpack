c6e906f4b68a594f135bfbb4a999daaa22c326a4
Liviu Ionescu <ilg@livius.net>
2025-05-20 23:38:34 +0300

    website re-generate commons

--- website/doxygen/top-common.h
+++ website/doxygen/top-common.h
@@ -2,14 +2,14 @@
  * DO NOT EDIT!
  * Automatically generated from docusaurus-template-liquid/templates/docusaurus.
  *
- * This file is part of the µOS++ project (https://micro-os-plus.github.io/).
+ * This file is part of the µOS++ project (https://micro-os-plus.github.com).
  * Copyright (c) 2021 Liviu Ionescu. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software
  * for any purpose is hereby granted, under the terms of the MIT license.
  *
  * If a copy of the license was not distributed with this file, it can
- * be obtained from https://opensource.org/licenses/mit.
+ * be obtained from https://opensource.org/licenses/MIT.
  */
 
 /**
