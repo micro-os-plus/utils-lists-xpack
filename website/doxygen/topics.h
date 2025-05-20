5c38fe0a230d6812bb93ac6b8027379ddd446281
Liviu Ionescu <ilg@livius.net>
2025-05-20 17:24:47 +0300

    website doxygen split into separate .dox files

--- website/doxygen/topics.h
+++ website/doxygen/topics.h
@@ -11,98 +11,23 @@
 
 /**
 
-@file
-@brief The Doxygen project specific topics definitions.
+@include{doc} files-and-folders/topics-h.dox
+@include{doc} files-and-folders/include.dox
 
-@defgroup micro-os-plus-utils-lists µOS++ Intrusive Lists
-@brief The µOS++ Intrusive Lists project topics.
-@ingroup micro-os-plus
-@details
+@include{doc} files-and-folders/include-micro-os-plus.dox
+@include{doc} files-and-folders/include-micro-os-plus-utils.dox
+@include{doc} files-and-folders/src.dox
 
-The **µOS++ Intrusive Lists** project is a C++ source code library
-designed to implement **µOS++ intrusive doubly linked lists**. These
-specialised lists eliminate the need for dynamic memory allocations
-by embedding linkage directly within the list elements, making them
-particularly advantageous for embedded systems.
+# Note: the order is important, it gives the order in the sidebar.
 
-@defgroup micro-os-plus-utils-lists-cpp-api C++ API
-@brief The µOS++ Intrusive Lists C++ API topics.
-@ingroup micro-os-plus-utils-lists
-@details
+@include{doc} topics/utils-lists.dox
+@include{doc} topics/cpp-api.dox
+@include{doc} topics/c-api.dox
 
-The µOS++ **utils-list** implementation uses C++ templates,
-thus the main API is C++.
-
-@defgroup micro-os-plus-utils-lists-c-api C API
-@brief The µOS++ Intrusive Lists C API topics (hint: none).
-@ingroup micro-os-plus-utils-lists
-@details
-
-The µOS++ **utils-list** project is specifically designed for C++ and does
-not include equivalent definitions for the C programming language.
-
-@defgroup micro-os-plus-utils-lists-intrusive-lists Intrusive Doubly Linked Lists
-@brief The µOS++ intrusive doubly linked lists classes.
-@ingroup micro-os-plus-utils-lists-cpp-api
-@details
-
-Intrusive lists are doubly linked lists that keep the two pointers inside
-each linked object.
-
-The intrusive lists can be defined by instantiating the
-`micro_os_plus::utils::intrusive_list` class template.
-
-@par Example
-
-```cpp
-#include <micro-os-plus/utils/lists.h>
-
-namespace os = micro_os_plus;
-
-class child
-{
-public:
-  child (const char* name);
-  // ...
-protected:
-  const char* name_;
-
-public:
-  // Intrusive node used to link this child to the registry list.
-  // Must be public.
-  os::utils::double_list_links registry_links_;
-};
-
-using static_children_list = os::utils::intrusive_list<
-        child, // type of nodes in the list
-        decltype (child::registry_links_), // type of the `registry_links_` member
-        &child::registry_links_, // name of member
-        static_double_list_links>; // type of the head links node
-
-// The list head is statically allocated.
-static_children_list kids_registry;
-```
-
-@defgroup micro-os-plus-utils-lists-double-lists Doubly Linked Lists
-@brief The µOS++ doubly linked lists classes.
-@ingroup micro-os-plus-utils-lists-cpp-api
-@details
-
-Traditional doubly linked or low intrusive doubly linked lists.
-
-For simpler use cases, like traditional list or low intrusive lists,
-there are several simpler classes and templates.
-
-@dir include/micro-os-plus/utils
-@brief The folder with the utilities header files.
-
-@dir include
-@brief The top folder with all header files.
-
-@dir include/micro-os-plus
-@brief The top folder with the µOS++ header files.
-
-@dir src
-@brief The top folder with the project source files.
+@addtogroup micro-os-plus-utils-lists-cpp-api
+@{
+  @include{doc} topics/cpp-api/intrusive-lists.dox
+  @include{doc} topics/cpp-api/double-lists.dox
+@}
 
 */
