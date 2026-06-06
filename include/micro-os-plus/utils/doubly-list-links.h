b00d283f0b8ae73aac546a956795d98a4ae440d5
Liviu Ionescu <ilg@livius.net>
2026-06-06 21:42:41 +0300

    use __has_include(<micro-os-plus/config.h>)

--- include/micro-os-plus/utils/doubly-list-links.h
+++ include/micro-os-plus/utils/doubly-list-links.h
@@ -33,9 +33,9 @@
 
 // ----------------------------------------------------------------------------
 
-#if defined(MICRO_OS_PLUS_INCLUDE_CONFIG_H)
+#if __has_include(<micro-os-plus/config.h>)
 #include <micro-os-plus/config.h>
-#endif // MICRO_OS_PLUS_INCLUDE_CONFIG_H
+#endif // __has_include(<micro-os-plus/config.h>)
 
 #if defined(MICRO_OS_PLUS_TRACE)
 #include <micro-os-plus/diag/trace.h>
