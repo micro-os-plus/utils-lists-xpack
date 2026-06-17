486157a9d8fc2266066063813ee3dfd4002ff08e
Liviu Ionescu <ilg@livius.net>
2026-06-17 21:00:19 +0300

    re-generate cmake & meson

--- tests/platforms/native/cmake/dependencies-folders.cmake
+++ tests/platforms/native/cmake/dependencies-folders.cmake
@@ -28,9 +28,11 @@ message (
 
 set (
   xpack_dependencies_folders
+  #
   # Portable dependencies.
   "${CMAKE_SOURCE_DIR}/xpacks/@micro-os-plus/diag-trace"
   "${CMAKE_SOURCE_DIR}/xpacks/@micro-os-plus/micro-test-plus"
+  #
   # Platform specific dependencies.
   "${CMAKE_BINARY_DIR}/xpacks/@micro-os-plus/architecture-synthetic-posix"
 )
