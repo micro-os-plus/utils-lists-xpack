741c694fd0b79791a88bb8e327aa51fcbab77fc0
Liviu Ionescu <ilg@livius.net>
2026-06-20 23:44:12 +0300

    lists.h fix c++20 test

--- include/micro-os-plus/utils/lists.h
+++ include/micro-os-plus/utils/lists.h
@@ -63,9 +63,11 @@
 
 // ----------------------------------------------------------------------------
 
+#if defined(__cplusplus)
 #if !(__cplusplus >= 202002L || (defined(_MSVC_LANG) && _MSVC_LANG >= 202002L))
 #error "C++20 or higher is required"
 #endif
+#endif // defined(__cplusplus)
 
 #if __has_include(<micro-os-plus/project-config.h>)
 #include <micro-os-plus/project-config.h>
