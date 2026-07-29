8c871b7472eee43c5365337dcec5aa0b1a829a7b
Liviu Ionescu <ilg@livius.net>
2026-07-29 13:19:57 +0300

    fix #endif // defined

--- include/micro-os-plus/utils/lists/inlines/intrusive-list-inlines.h
+++ include/micro-os-plus/utils/lists/inlines/intrusive-list-inlines.h
@@ -283,7 +283,7 @@ namespace micro_os_plus::utils
   {
 #if defined(MICRO_OS_PLUS_UTILS_LISTS_TRACE_CONSTRUCTORS_ENABLED)
     trace::printf ("%s() @%p \n", __func__, static_cast<const void*> (this));
-#endif // MICRO_OS_PLUS_UTILS_LISTS_TRACE_CONSTRUCTORS_ENABLED
+#endif // defined(MICRO_OS_PLUS_UTILS_LISTS_TRACE_CONSTRUCTORS_ENABLED)
   }
 
   /**
@@ -299,7 +299,7 @@ namespace micro_os_plus::utils
   {
 #if defined(MICRO_OS_PLUS_UTILS_LISTS_TRACE_CONSTRUCTORS_ENABLED)
     trace::printf ("%s() @%p \n", __func__, static_cast<const void*> (this));
-#endif // MICRO_OS_PLUS_UTILS_LISTS_TRACE_CONSTRUCTORS_ENABLED
+#endif // defined(MICRO_OS_PLUS_UTILS_LISTS_TRACE_CONSTRUCTORS_ENABLED)
   }
 
   /**
@@ -353,7 +353,7 @@ namespace micro_os_plus::utils
 #if defined(MICRO_OS_PLUS_UTILS_LISTS_TRACE_ENABLED)
     trace::printf ("%s() @%p %p\n", __func__, static_cast<const void*> (this),
                    static_cast<const void*> (&node));
-#endif // !defined(MICRO_OS_PLUS_UTILS_LISTS_TRACE_ENABLED)
+#endif // defined(MICRO_OS_PLUS_UTILS_LISTS_TRACE_ENABLED)
 
     // The assert(links_.initialised()) is checked by the L class.
 
@@ -386,7 +386,7 @@ namespace micro_os_plus::utils
 #if defined(MICRO_OS_PLUS_UTILS_LISTS_TRACE_ENABLED)
     trace::printf ("%s() @%p %p\n", __func__, static_cast<const void*> (this),
                    static_cast<const void*> (&node));
-#endif // !defined(MICRO_OS_PLUS_UTILS_LISTS_TRACE_ENABLED)
+#endif // defined(MICRO_OS_PLUS_UTILS_LISTS_TRACE_ENABLED)
 
     // The assert(links_.initialised()) is checked by the L class.
 
@@ -422,7 +422,7 @@ namespace micro_os_plus::utils
   {
 #if defined(MICRO_OS_PLUS_UTILS_LISTS_TRACE_ENABLED)
     trace::printf ("%s() @%p\n", __func__, static_cast<const void*> (this));
-#endif // !defined(MICRO_OS_PLUS_UTILS_LISTS_TRACE_ENABLED)
+#endif // defined(MICRO_OS_PLUS_UTILS_LISTS_TRACE_ENABLED)
 
     // The assert(links_.initialised()) is checked by the L class.
 
@@ -445,7 +445,7 @@ namespace micro_os_plus::utils
   {
 #if defined(MICRO_OS_PLUS_UTILS_LISTS_TRACE_ENABLED)
     trace::printf ("%s() @%p\n", __func__, static_cast<const void*> (this));
-#endif // !defined(MICRO_OS_PLUS_UTILS_LISTS_TRACE_ENABLED)
+#endif // defined(MICRO_OS_PLUS_UTILS_LISTS_TRACE_ENABLED)
 
     // The assert would probably be redundant, since it was
     // already tested in `begin()`.
@@ -531,7 +531,7 @@ namespace micro_os_plus::utils
   {
 #if defined(MICRO_OS_PLUS_UTILS_LISTS_TRACE_ENABLED)
     trace::printf ("%s() @%p\n", __func__, static_cast<const void*> (this));
-#endif // !defined(MICRO_OS_PLUS_UTILS_LISTS_TRACE_ENABLED)
+#endif // defined(MICRO_OS_PLUS_UTILS_LISTS_TRACE_ENABLED)
 
     if (empty ())
       {
@@ -560,7 +560,7 @@ namespace micro_os_plus::utils
   {
 #if defined(MICRO_OS_PLUS_UTILS_LISTS_TRACE_ENABLED)
     trace::printf ("%s() @%p\n", __func__, static_cast<const void*> (this));
-#endif // !defined(MICRO_OS_PLUS_UTILS_LISTS_TRACE_ENABLED)
+#endif // defined(MICRO_OS_PLUS_UTILS_LISTS_TRACE_ENABLED)
 
     if (empty ())
       {
