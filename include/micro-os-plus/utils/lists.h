b657813dea2a24b385851c172f04ffbf2d28687f
Liviu Ionescu <ilg@livius.net>
2026-07-30 13:36:42 +0300

    website update .dox

--- include/micro-os-plus/utils/lists.h
+++ include/micro-os-plus/utils/lists.h
@@ -29,35 +29,6 @@
  * and @ref intrusive-list-inlines.h.
  */
 
-/**
- * @mainpage
- * This library implements several **doubly linked lists**, utilised by various
- * **µOS++** components to manage internal objects. Its design is sufficiently
- * **generic** to be valuable in other applications, warranting its packaging
- * as a standalone library.
- *
- * A key differentiator from `std::list` is that this implementation **does not
- * require dynamic memory allocation** for list links, thereby eliminating the
- * need for an allocator. Instead, it employs **intrusive lists**, where links
- * are stored directly within the list elements.
- *
- * Another notable feature is the support for **statically initialised lists**,
- * which can be created in the **global scope** without modifying their members
- * in constructors. These lists are fully **initialised** by setting their
- * entire content to zero during startup (via **BSS initialisation**).
- *
- * This approach enables **static objects** to **auto-register** with static
- * registrar instances. As the execution order of static constructors is
- * undefined, the only reliable way to ensure the registrar is ready before
- * clients attempt to register is to initialise it during **startup** (via BSS
- * initialisation) prior to the execution of static constructors.
- *
- * [Note: For comparison, the original Doxygen html pages, continue to be
- * available via the <a
- * href="https://micro-os-plus.github.io/utils-lists-xpack/doxygen/index.html">doxygen/\*</a>
- * URLs.]
- */
-
 #ifndef MICRO_OS_PLUS_UTILS_LISTS_H_
 #define MICRO_OS_PLUS_UTILS_LISTS_H_
 
--- website/doxygen/topics.h
+++ website/doxygen/topics.h
@@ -1,5 +1,5 @@
 /*
- * This file is part of the µOS++ project (https://micro-os-plus.github.com/).
+ * This file is part of the µOS++ project (https://micro-os-plus.github.io/).
  * Copyright (c) 2021-2026 Liviu Ionescu. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software
@@ -15,19 +15,39 @@
 
 /**
 
-@include{doc} files-and-folders/topics-h.dox
+@include{doc} pages/mainpage.dox
 
-@include{doc} files-and-folders/include-micro-os-plus-utils.dox
-@include{doc} files-and-folders/src.dox
+@include{doc} files/topics-h.dox
 
-@include{doc} topics/utils-lists.dox
-@include{doc} topics/cpp-api.dox
-@include{doc} topics/c-api.dox
+@include{doc} folders/include-micro-os-plus-utils.dox
+@include{doc} folders/include-micro-os-plus-utils-lists.dox
+@include{doc} folders/include-micro-os-plus-utils-lists-inlines.dox
+
+@include{doc} folders/src.dox
+
+@addtogroup micro-os-plus
+@{
+  @include{doc} topics/utils-lists.dox
+@}
+
+@addtogroup micro-os-plus-utils-lists
+@{
+  @include{doc} topics/cpp-api.dox
+  @include{doc} topics/c-api.dox
+  @include{doc} topics/config.dox
+@}
 
 @addtogroup micro-os-plus-utils-lists-cpp-api
 @{
   @include{doc} topics/cpp-api/intrusive-lists.dox
   @include{doc} topics/cpp-api/doubly-lists.dox
+@}
+
+@addtogroup micro-os-plus-utils-lists-config
+@{
+  @include{doc} macros/utils-lists-enabled.dox
+  @include{doc} macros/utils-lists-trace-enabled.dox
+  @include{doc} macros/utils-lists-trace-constructors-enabled.dox
 @}
 
 */
